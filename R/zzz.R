bf7ec3787bdbf6a622f3b476d3d62bfbda1aa30b
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-07-08 14:48:08 +0200

    updated start-up message

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,9 @@
 .onAttach = function(lib, pkg) {
   # startup message
   msg = c(paste0(
-    "ao version ", packageVersion("ao")),
-    "\nType 'citation(\"ao\")' for citing this R package in publications.",
-    "\nSee https://github.com/loelschlaeger/ao for references.")
+    "Thanks for installing ao version ",packageVersion("ao")),".",
+    "\nType 'citation(\"ao\")' for citing this R package in your publications.",
+    "\nThe project is stored at https://github.com/loelschlaeger/ao.")
   packageStartupMessage(msg)
   invisible()
 }
