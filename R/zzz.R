fa7a07d307f46e4f1fab74c4c237787db1a00a6f
loelschlaeger <lennart.oelschlaeger@uni-bielefeld.de>
2021-08-10 15:38:59 +0200

    updated startup message

--- R/zzz.R
+++ R/zzz.R
@@ -3,7 +3,7 @@
   msg = c(paste0(
     "Thanks for installing ao version ",packageVersion("ao")),".",
     "\nType 'citation(\"ao\")' for citing this R package in your publications.",
-    "\nThe project is stored at https://github.com/loelschlaeger/ao.")
+    "\nHappy alternating optimization!")
   packageStartupMessage(msg)
   invisible()
 }
